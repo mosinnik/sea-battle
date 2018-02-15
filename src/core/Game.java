b44f37329d4327d1382b4c2542290d00b7148a1d
nick.mosin <nick.mosin@aisa.ru>
2018-02-15 13:12:44 +0300

    some refactoring

--- src/core/Game.java
+++ src/core/Game.java
@@ -3,7 +3,6 @@ package core;
 import DAO.MemoryDAO;
 import core.arrays.BooleanArray;
 import core.arrays.CoordinateArray;
-import core.characters.Player;
 
 import java.io.Serializable;
 
@@ -11,40 +10,40 @@ public class Game implements Serializable
 {
 	public Game(long firstPlayerId, long secondPlayerId, int mapSize)
 	{
-		map1=new GameMap(mapSize, firstPlayerId);
-		map2=new GameMap(mapSize, secondPlayerId);
-		printMap1=new int[mapSize][mapSize];
-		printMap2=new int[mapSize][mapSize];
+		map1 = new GameMap(mapSize, firstPlayerId);
+		map2 = new GameMap(mapSize, secondPlayerId);
+		printMap1 = new int[mapSize][mapSize];
+		printMap2 = new int[mapSize][mapSize];
 
-		checkMap1=new int[mapSize][mapSize];
-		checkMap2=new int[mapSize][mapSize];
-		for(int i=0;i<mapSize;i++)
-			for(int k=0;k<mapSize;k++)
+		checkMap1 = new int[mapSize][mapSize];
+		checkMap2 = new int[mapSize][mapSize];
+		for(int i = 0; i < mapSize; i++)
+			for(int k = 0; k < mapSize; k++)
 			{
-				checkMap1[i][k]=0;
-				checkMap2[i][k]=0;
+				checkMap1[i][k] = 0;
+				checkMap2[i][k] = 0;
 			}
-		this.firstPlayerId=firstPlayerId;
-		this.secondPlayerId=secondPlayerId;
-		this.isSecondPlayerAI=MemoryDAO.getInstance().getPlayer(secondPlayerId).isAI();
-		this.mapSize=mapSize;
-		step=true;
-		state=true;
-		time=0;
-		winFirst=true;
-		round=0;
-		id=gameId;
+		this.firstPlayerId = firstPlayerId;
+		this.secondPlayerId = secondPlayerId;
+		this.isSecondPlayerAI = MemoryDAO.getInstance().getPlayer(secondPlayerId).isAI();
+		this.mapSize = mapSize;
+		step = true;
+		state = true;
+		time = 0;
+		winFirst = true;
+		round = 0;
+		id = gameId;
 		Game.incGameId();
 	}
 
 	public void setShip(CoordinateArray coordinates, int length, long player)
 	{
-		if(player==map1.getPlayerId())
+		if(player == map1.getPlayerId())
 		{
 			map1.setShip(coordinates, length);
 			setShipCheck(coordinates, length, player);
 		}
-		else if(player==map2.getPlayerId())
+		else if(player == map2.getPlayerId())
 		{
 			map2.setShip(coordinates, length);
 			setShipCheck(coordinates, length, player);
@@ -53,18 +52,18 @@ public class Game implements Serializable
 
 	public Ship getShip(int i, int k, long player)
 	{
-		if(player==map1.getPlayerId())
+		if(player == map1.getPlayerId())
 			return map2.getShip(i, k);
-		else if(player==map2.getPlayerId())
+		else if(player == map2.getPlayerId())
 			return map1.getShip(i, k);
 		return null;
 	}
 
 	public void resetMap(long player)
 	{
-		if(player==map1.getPlayerId())
+		if(player == map1.getPlayerId())
 			map1.resetMap();
-		else if(player==map2.getPlayerId())
+		else if(player == map2.getPlayerId())
 			map2.resetMap();
 	}
 
@@ -72,30 +71,30 @@ public class Game implements Serializable
 	{
 		//b[0]-is fire successful
 		//b[1]-is fire kill whole ship (need for AI)
-		BooleanArray b=new BooleanArray();
+		BooleanArray b = new BooleanArray();
 		if(state)
 		{
 			round++;
-			b=fireM(i, k, player);
+			b = fireM(i, k, player);
 			if(b.isShotSuccessful())
 			{
-				if(player==map1.getPlayerId())
+				if(player == map1.getPlayerId())
 				{
 					map2.decNumbOfDecks();
-					if(map2.getNumbOfDecks()==0)
+					if(map2.getNumbOfDecks() == 0)
 					{
-						state=false;
-						winFirst=true;
+						state = false;
+						winFirst = true;
 					}
 					return b;
 				}
-				else if(player==map2.getPlayerId())
+				else if(player == map2.getPlayerId())
 				{
 					map1.decNumbOfDecks();
-					if(map1.getNumbOfDecks()==0)
+					if(map1.getNumbOfDecks() == 0)
 					{
-						state=false;
-						winFirst=false;
+						state = false;
+						winFirst = false;
 					}
 					return b;
 				}
@@ -105,7 +104,7 @@ public class Game implements Serializable
 		}
 		else
 		{
-			System.out.println("By state:Game is finished. "+(winFirst ? "First" : "Second")+" player win.");
+			System.out.println("By state:Game is finished. " + (winFirst ? "First" : "Second") + " player win.");
 		}
 		return b;
 	}
@@ -113,14 +112,14 @@ public class Game implements Serializable
 	public BooleanArray fireM(int i, int k, long player)
 	{
 
-		BooleanArray b=new BooleanArray();
-		if(player==map2.getPlayerId())
+		BooleanArray b = new BooleanArray();
+		if(player == map2.getPlayerId())
 		{
-			if(map1.getShip(i, k)!=null)
+			if(map1.getShip(i, k) != null)
 			{
-				if(map1.destroy(i, k)!=-1)
+				if(map1.destroy(i, k) != -1)
 				{
-					printMap2[i][k]=2;
+					printMap2[i][k] = 2;
 					b.shotSuccessful();
 					if(map1.getShip(i, k).isDown())
 						b.shipDown();
@@ -129,16 +128,16 @@ public class Game implements Serializable
 			else
 			{
 				map1.setSea(i, k);
-				printMap2[i][k]=3;
+				printMap2[i][k] = 3;
 			}
 		}
-		else if(player==map1.getPlayerId())
+		else if(player == map1.getPlayerId())
 		{
-			if(map2.getShip(i, k)!=null)
+			if(map2.getShip(i, k) != null)
 			{
-				if(map2.destroy(i, k)!=-1)
+				if(map2.destroy(i, k) != -1)
 				{
-					printMap1[i][k]=2;
+					printMap1[i][k] = 2;
 					b.shotSuccessful();
 					if(map2.getShip(i, k).isDown())
 						b.shipDown();
@@ -147,7 +146,7 @@ public class Game implements Serializable
 			else
 			{
 				map2.setSea(i, k);
-				printMap1[i][k]=3;
+				printMap1[i][k] = 3;
 			}
 		}
 		return b;
@@ -155,9 +154,9 @@ public class Game implements Serializable
 
 	public long[] getPlayersIds()
 	{
-		long[] l=new long[2];
-		l[0]=firstPlayerId;
-		l[1]=secondPlayerId;
+		long[] l = new long[2];
+		l[0] = firstPlayerId;
+		l[1] = secondPlayerId;
 		return l;
 	}
 
@@ -193,41 +192,41 @@ public class Game implements Serializable
 
 	public boolean isPlayersIds(long id1, long id2)
 	{
-		if(firstPlayerId==id1 && secondPlayerId==id2 || firstPlayerId==id2 && secondPlayerId==id1)
+		if(firstPlayerId == id1 && secondPlayerId == id2 || firstPlayerId == id2 && secondPlayerId == id1)
 			return true;
 		return false;
 	}
 
 	public int[][] getMap(long id)
 	{
-		int[][] ik=new int[mapSize][mapSize];
+		int[][] ik = new int[mapSize][mapSize];
 		GameMap map;
-		if(id==firstPlayerId)
-			map=map1;
+		if(id == firstPlayerId)
+			map = map1;
 		else
-			map=map2;
+			map = map2;
 
-		for(int i=0;i<mapSize;i++)
-			for(int k=0;k<mapSize;k++)
+		for(int i = 0; i < mapSize; i++)
+			for(int k = 0; k < mapSize; k++)
 			{
-				Ship sh=map.getShip(i, k);
-				if(sh!=null)
+				Ship sh = map.getShip(i, k);
+				if(sh != null)
 					if(!sh.isSea())
 						if(sh.getState(i, k))
-							ik[i][k]=1;//System.out.print("|X");
+							ik[i][k] = 1;//System.out.print("|X");
 						else
-							ik[i][k]=2;//System.out.print("|Z");
+							ik[i][k] = 2;//System.out.print("|Z");
 					else
-						ik[i][k]=3;//System.out.print("|S");
+						ik[i][k] = 3;//System.out.print("|S");
 				else
-					ik[i][k]=0;//System.out.print("|_");
+					ik[i][k] = 0;//System.out.print("|_");
 			}
 		return ik;
 	}
 
 	public int[][] getPrintMap(long id)
 	{
-		if(id==firstPlayerId)
+		if(id == firstPlayerId)
 			return printMap1;
 		else
 			return printMap2;
@@ -235,7 +234,7 @@ public class Game implements Serializable
 
 	public int[][] getCheckMap(long id)
 	{
-		if(id==firstPlayerId)
+		if(id == firstPlayerId)
 			return checkMap1;
 		else
 			return checkMap2;
@@ -243,83 +242,83 @@ public class Game implements Serializable
 
 	public int getCountShip()
 	{
-		int n, sum=0, p=0;
-		int s=mapSize*mapSize;
-		for(n=1;n<=mapSize;n++)
+		int n, sum = 0, p = 0;
+		int s = mapSize * mapSize;
+		for(n = 1; n <= mapSize; n++)
 		{
-			sum=0;
-			p=0;
-			for(int i=1;i<=n;i++)
+			sum = 0;
+			p = 0;
+			for(int i = 1; i <= n; i++)
 			{
-				p+=i*(n-i+1);
-				for(int k=0;k<i;k++)
-					sum+=9+3*k;
+				p += i * (n - i + 1);
+				for(int k = 0; k < i; k++)
+					sum += 9 + 3 * k;
 			}
-			if(sum>s)
+			if(sum > s)
 				break;
 		}
 
-		if((double)p*100/s>25)
-			n=n-1;
+		if((double)p * 100 / s > 25)
+			n = n - 1;
 
 		return n;
 	}
 
 	public boolean checkShipCoordinates(CoordinateArray coordinates, int length, long player)
 	{
-		int di=0, dk=0;
+		int di = 0, dk = 0;
 
-		int i=coordinates.getI();
-		int k=coordinates.getK();
+		int i = coordinates.getI();
+		int k = coordinates.getK();
 		int[][] map;
 
-		if(player==map1.getPlayerId())
-			map=checkMap1;
+		if(player == map1.getPlayerId())
+			map = checkMap1;
 		else
-			map=checkMap2;
+			map = checkMap2;
 
 		if(coordinates.getDirection())
 		{
-			if(i+length>mapSize)
+			if(i + length > mapSize)
 				return false;
-			di=1;
+			di = 1;
 		}
 		else
 		{
-			if(k+length>mapSize)
+			if(k + length > mapSize)
 				return false;
-			dk=1;
+			dk = 1;
 		}
-		for(int x=0;x<length;x++)
+		for(int x = 0; x < length; x++)
 		{
-			if(map[i][k]!=0)
+			if(map[i][k] != 0)
 				return false;
-			i+=di;
-			k+=dk;
+			i += di;
+			k += dk;
 		}
 		return true;
 	}
 
 	void setShipCheck(CoordinateArray coordinates, int length, long player)
 	{
-		int di=0;
-		int dk=0;
+		int di = 0;
+		int dk = 0;
 
-		int i=coordinates.getI();
-		int k=coordinates.getK();
+		int i = coordinates.getI();
+		int k = coordinates.getK();
 		int[][] map;
 
-		if(player==map1.getPlayerId())
-			map=checkMap1;
+		if(player == map1.getPlayerId())
+			map = checkMap1;
 		else
-			map=checkMap2;
+			map = checkMap2;
 
 		if(coordinates.getDirection())
-			di=1;
+			di = 1;
 		else
-			dk=1;
+			dk = 1;
 
-		for(int x=0;x<length;x++)
+		for(int x = 0; x < length; x++)
 		{
 			//   ###################
 			//   ######k-1# k #k+1##
@@ -331,85 +330,83 @@ public class Game implements Serializable
 			//   ##i+1# 6 # 7 # 8 ##
 			//   ###################
 
-			if(i!=0)
+			if(i != 0)
 			{
-				if(k!=0)
-					map[i-1][k-1]=1;
-				if(k!=mapSize-1)
-					map[i-1][k+1]=1;
-				map[i-1][k]=1;
+				if(k != 0)
+					map[i - 1][k - 1] = 1;
+				if(k != mapSize - 1)
+					map[i - 1][k + 1] = 1;
+				map[i - 1][k] = 1;
 			}
-			if(i!=mapSize-1)
+			if(i != mapSize - 1)
 			{
-				if(k!=0)
-					map[i+1][k-1]=1;
-				if(k!=mapSize-1)
-					map[i+1][k+1]=1;
-				map[i+1][k]=1;
+				if(k != 0)
+					map[i + 1][k - 1] = 1;
+				if(k != mapSize - 1)
+					map[i + 1][k + 1] = 1;
+				map[i + 1][k] = 1;
 			}
-			if(k!=0)
-				map[i][k-1]=1;
-			if(k!=mapSize-1)
-				map[i][k+1]=1;
-			map[i][k]=1;
+			if(k != 0)
+				map[i][k - 1] = 1;
+			if(k != mapSize - 1)
+				map[i][k + 1] = 1;
+			map[i][k] = 1;
 
-			i+=di;
-			k+=dk;
+			i += di;
+			k += dk;
 		}
+	}
+
+	private String fieldStateSign(int fieldState)
+	{
+		switch(fieldState)
+		{
+			case 0:
+				return " ";
+			case 1:
+				return "X";
+			case 2:
+				return "Z";
+			case 3:
+				return "S";
+		}
+		throw new RuntimeException("Unknown field state " + fieldState);
 	}
 
 	public void printGame(boolean printFirstPlayer, boolean printSecondPlayer)
 	{
-		int size=getMapSize();
-		int[][] map1=getMap(getPlayersIds()[0]);
-		int[][] map2=getMap(getPlayersIds()[1]);
-		int[][] map1m=getPrintMap(getPlayersIds()[0]);
-		int[][] map2m=getPrintMap(getPlayersIds()[1]);
-		long round=getRound();
+		int size = getMapSize();
+		int[][] map1 = getMap(getPlayersIds()[0]);
+		int[][] map2 = getMap(getPlayersIds()[1]);
+		int[][] map1m = getPrintMap(getPlayersIds()[0]);
+		int[][] map2m = getPrintMap(getPlayersIds()[1]);
+		long round = getRound();
 
-		System.out.print("=== "+MemoryDAO.getInstance().getPlayer(getPlayersIds()[0]).getName()+" vs ");
-		System.out.println(MemoryDAO.getInstance().getPlayer(getPlayersIds()[1]).getName()+" == round "+round+" ===");
+		System.out.print("=== " + MemoryDAO.getInstance().getPlayer(getPlayersIds()[0]).getName() + " vs ");
+		System.out.println(MemoryDAO.getInstance().getPlayer(getPlayersIds()[1]).getName() + " == round " + round + " ===");
 
 //first player screen
 		if(printFirstPlayer)
 		{
 			System.out.print("  ");
-			for(int i=0;i<size;i++)
-				System.out.print(" "+i);
+			for(int i = 0; i < size; i++)
+				System.out.print(" " + i);
 			System.out.print("            ");
-			for(int i=0;i<size;i++)
-				System.out.print(" "+i);
+			for(int i = 0; i < size; i++)
+				System.out.print(" " + i);
 			System.out.println("  ");
 
 
-			for(int i=0;i<size;i++)
+			for(int i = 0; i < size; i++)
 			{
-				System.out.print(" "+i);
-				for(int k=0;k<size;k++)
-					if(map1[i][k]!=0)
-						if(map1[i][k]!=3)
-							if(map1[i][k]==1)
-								System.out.print("|X");
-							else
-								System.out.print("|Z");
-						else
-							System.out.print("|S");
-					else
-						System.out.print("|_");
+				System.out.print(String.format("%3d", i));
+				for(int k = 0; k < size; k++)
+					System.out.print("|" + fieldStateSign(map1[i][k]));
 				System.out.print("|");
 
-				System.out.print("          "+i);
-				for(int k=0;k<size;k++)
-					if(map1m[i][k]!=0)
-						if(map1m[i][k]!=3)
-							if(map1m[i][k]==1)
-								System.out.print("|X");
-							else
-								System.out.print("|Z");
-						else
-							System.out.print("|S");
-					else
-						System.out.print("|_");
+				System.out.print("         " + String.format("%3d", i));
+				for(int k = 0; k < size; k++)
+					System.out.print("|" + fieldStateSign(map1m[i][k]));
 				System.out.println("|");
 			}
 		}
@@ -418,42 +415,25 @@ public class Game implements Serializable
 		if(printSecondPlayer)
 		{
 			System.out.print("  ");
-			for(int i=0;i<size;i++)
-				System.out.print(" "+i);
+			for(int i = 0; i < size; i++)
+				System.out.print(" " + i);
 			System.out.print("            ");
-			for(int i=0;i<size;i++)
-				System.out.print(" "+i);
+			for(int i = 0; i < size; i++)
+				System.out.print(" " + i);
 			System.out.println("  ");
 
-			for(int i=0;i<size;i++)
+			for(int i = 0; i < size; i++)
 			{
 				System.out.print(String.format("%3d", i));
-				for(int k=0;k<size;k++)
-					if(map2[i][k]!=0)
-						if(map2[i][k]!=3)
-							if(map2[i][k]==1)
-								System.out.print("|X");
-							else
-								System.out.print("|Z");
-						else
-							System.out.print("|S");
-					else
-						System.out.print("|_");
+				for(int k = 0; k < size; k++)
+					System.out.print("|" + fieldStateSign(map2[i][k]));
 
 				System.out.print("|");
 
 				System.out.print("         " + String.format("%3d", i));
-				for(int k=0;k<size;k++)
-					if(map2m[i][k]!=0)
-						if(map2m[i][k]!=3)
-							if(map2m[i][k]==1)
-								System.out.print("|X");
-							else
-								System.out.print("|Z");
-						else
-							System.out.print("|S");
-					else
-						System.out.print("|_");
+				for(int k = 0; k < size; k++)
+					System.out.print("|" + fieldStateSign(map2m[i][k]));
+
 				System.out.println("|");
 			}
 		}
@@ -466,7 +446,7 @@ public class Game implements Serializable
 
 	public void setStep(boolean step)
 	{
-		       this.step=step;
+		this.step = step;
 	}
 
 	public boolean getStep()
@@ -477,7 +457,7 @@ public class Game implements Serializable
 	public void changeStep()
 	{
 //change step between players 
-		step=!step;
+		step = !step;
 	}
 
 
